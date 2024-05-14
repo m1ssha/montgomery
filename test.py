4b00de0d4550a8f6e202798238c122ac7cd91d57
m1ssha <m1ssha@vk.com>
2024-05-14 14:19:00 +0300

    добавлена возможность тестирования через test.py. Не является компонентом программы

---
+++ test.py
@@ -0,0 +1,42 @@
+import subprocess
+import random
+import openpyxl
+
+
+def run_main_with_args(a, b, n):
+    command = f"venv\Scripts\python.exe main.py {a} {b} {n}"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    process.wait()
+    output, _ = process.communicate()
+    return output.decode('utf-8')
+
+
+# Создаем новую книгу Excel
+workbook = openpyxl.Workbook()
+sheet = workbook.active
+sheet.title = "Results"
+sheet.append(["a", "b", "n", "Result", "Execution Time", "Memory Usage"])
+
+
+if __name__ == "__main__":
+    for _ in range(100):
+        a = random.randint(1, 10000000000)
+        b = random.randint(1, 10000000000)
+        n = random.randint(1, 1000000)
+
+        if n % 2 != 0:
+            output = run_main_with_args(a, b, n)
+            lines = output.strip().split('\n')
+
+            if len(lines) >= 3:
+                result_line = lines[-3]
+                time_line = lines[-2]
+                memory_line = lines[-1]
+
+                result = result_line.split(": ")[1]
+                execution_time = time_line.split(": ")[1].split(" ")[0]
+                memory_usage = memory_line.split(": ")[1].split(" ")[0]
+
+                sheet.append([a, b, n, result, execution_time, memory_usage])
+
+        workbook.save("results.xlsx")
