6552e38cf46f4dacfa9eb574bef0571de8fd73ce
m1ssha <m1ssha@vk.com>
2024-05-26 14:31:57 +0300

    в очередной раз переписан test.py

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ import psutil
 workbook = openpyxl.Workbook()
 sheet = workbook.active
 sheet.title = "Results"
-sheet.append(["a", "b", "n", "Result", "Execution Time (seconds)", "Memory Usage (mb)"])
+sheet.append(["a", "b", "n", "Result", "Время выполнения программы (сек)", "Использование памяти (мБ)"])
 
-bits = 200000
+bits = 150000
 
 
 if __name__ == "__main__":
@@ -21,13 +21,17 @@ if __name__ == "__main__":
         b = gmpy2.mpz(random.getrandbits(bits))
         n = gmpy2.mpz(random.getrandbits(bits))
 
-        if n % 2 != 0:
-            print(len(str(a)))
-            result = m.multiplication(a, b, n)
-            end_time = time.time()
-            execution_time = end_time - start_time
-            memory_usage = psutil.Process().memory_info().rss / 1024**2
+        print(f"Количество знаков в числе:\na: {len(str(a))}\nb: {len(str(b))}\nn: {len(str(n))}\n")
 
-            sheet.append([str(a), str(b), str(n), str(result), execution_time, memory_usage])
+        if n % 2 == 0:
+            n += 1
+
+        result = m.multiplication(a, b, n)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Время выполнения: {execution_time}")
+        memory_usage = psutil.Process().memory_info().rss / 1024**2
+
+        sheet.append([str(a), str(b), str(n), str(result), execution_time, memory_usage])
 
     workbook.save(f"results{bits}.xlsx")
