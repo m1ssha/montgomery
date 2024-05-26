7f2dae3ad0a8774dee04788caed697d3d462af8b
m1ssha <m1ssha@vk.com>
2024-05-26 13:47:12 +0300

    переписаны тесты. Теперь случайное число зависит от bits

--- test.py
+++ test.py
@@ -1,41 +1,33 @@
-import subprocess
+import multiplication as m
 import random
 import openpyxl
-
-
-def run_main_with_args(a, b, n):
-    command = f"venv\Scripts\python.exe main.py {a} {b} {n}"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    process.wait()
-    output, _ = process.communicate()
-    return output.decode('utf-8')
+import gmpy2
+import time
+import psutil
 
 
 workbook = openpyxl.Workbook()
 sheet = workbook.active
 sheet.title = "Results"
-sheet.append(["a", "b", "n", "Result", "Execution Time", "Memory Usage"])
+sheet.append(["a", "b", "n", "Result", "Execution Time (seconds)", "Memory Usage (mb)"])
+
+bits = 200000
 
 
 if __name__ == "__main__":
-    for _ in range(25):
-        a = random.randint(1, 10000000000000000000)
-        b = random.randint(1, 10000000000000000000)
-        n = random.randint(1, 99999999)
+    for _ in range(5):
+        start_time = time.time()
+        a = gmpy2.mpz(random.getrandbits(bits))
+        b = gmpy2.mpz(random.getrandbits(bits))
+        n = gmpy2.mpz(random.getrandbits(bits))
 
         if n % 2 != 0:
-            output = run_main_with_args(a, b, n)
-            lines = output.strip().split('\n')
+            print(len(str(a)))
+            result = m.multiplication(a, b, n)
+            end_time = time.time()
+            execution_time = end_time - start_time
+            memory_usage = psutil.Process().memory_info().rss / 1024**2
 
-            if len(lines) >= 3:
-                result_line = lines[-3]
-                time_line = lines[-2]
-                memory_line = lines[-1]
+            sheet.append([str(a), str(b), str(n), str(result), execution_time, memory_usage])
 
-                result = result_line.split(": ")[1]
-                execution_time = time_line.split(": ")[1].split(" ")[0]
-                memory_usage = memory_line.split(": ")[1].split(" ")[0]
-
-                sheet.append([a, b, n, result, execution_time, memory_usage])
-
-        workbook.save("results4.xlsx")
+    workbook.save(f"results{bits}.xlsx")
